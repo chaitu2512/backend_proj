527792a900ca36a9822ebd6eb93d36a4c07ae206
chaitu2512 <62876104+chaitu2512@users.noreply.github.com>
2024-09-17 22:09:05 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import app from "./app.js";
+// import app from "./app.js";
 
-dotenv.config({
-    path: './.env'
+// dotenv.config({
+//     path: './.env'
+// })
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
 })
-
-connectDB();
+.catch((err)=>{
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+})
 
 
 
