92f4ac984c619685562d116925f5bdd2290c4444
chaitu2512 <62876104+chaitu2512@users.noreply.github.com>
2024-09-17 23:01:56 +0530

    user and video model

--- src/app.js
+++ src/app.js
@@ -13,6 +13,6 @@ app.use(express.urlencoded({extended: true,limit: "16kb"}));
 app.use(express.static("public"))
 app.use(cookieParser());
 
-export {app};
+// export {app};
 
-// export default app
+export default app
